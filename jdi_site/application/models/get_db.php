06c979dac2c938a14acce7f11ddf69c70d3f145b
howlomogo <mattpilcher87@gmail.com>
2015-03-12 11:53:40 +0000

    Update get_db.php
    changed table name

--- jdi_site/application/models/get_db.php
+++ jdi_site/application/models/get_db.php
@@ -2,7 +2,7 @@
 
 	class Get_db extends CI_Model{
 		function getAll() {
-			$query = $this->db->query("SELECT * FROM sometable");
+			$query = $this->db->query("SELECT * FROM carstable");
 
 			// converts it into an array similar to fetch assoc
 			return $query->result();
