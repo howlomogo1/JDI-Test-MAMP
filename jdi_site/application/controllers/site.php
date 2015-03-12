890c7d552ee44666769fdc6522b9cfe1fc0c0185
howlomogo <mattpilcher87@gmail.com>
2015-03-12 12:03:40 +0000

    Update site.php

--- jdi_site/application/controllers/site.php
+++ jdi_site/application/controllers/site.php
@@ -13,9 +13,6 @@ class Site extends CI_Controller {
 		$this->load->model("get_db");
 
 		$somedata['results'] = $this->get_db->getAll();
-		//$this->load->view("view_db", $data);
-
-
 		$somedata['theTitle'] = "JDI Interview Website";
 		$somedata['theHeading'] = "Example Title";
 		$somedata['theParagraph'] = "An example of a sub heading";
@@ -28,11 +25,8 @@ class Site extends CI_Controller {
 	}
 
 	public function getValues() {
-		// This should be fine to pass into the hompage as far as sql goes.
 		$this->load->model("get_db");
 		$data['results'] = $this->get_db->getAll();
-		//header('Content-type: application/json');
-		//echo json_encode($this->get_db->getAll());
 		$this->load->view("view_db", $data);
 	}
 }
